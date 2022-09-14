749e7ef6f1109b7dbaf6ab60af9b7e8f58830ce7
amirisback <faisalamircs@gmail.com>
2022-09-15 01:31:23 +0700

    add: MainActivity.kt

---
+++ frogobox/research/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.frogobox.research
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Faisal Amir on 15/09/22
+ * -----------------------------------------
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) Frogobox ID / amirisback
+ * All rights reserved
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
